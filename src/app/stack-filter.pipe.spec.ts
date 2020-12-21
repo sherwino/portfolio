5e27150bd92a8b8dcbacd013286ef3f186b7b4f5
sherwino <s.garcia@applicaster.com>
2020-12-21 02:32:48 -0500

    feat: initial setup of simple site

--- src/app/stack-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { StackFilterPipe } from './stack-filter.pipe';
-
-describe('StackFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new StackFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
