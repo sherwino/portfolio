b120a18f29f973b5640e311358df501479369afb
sherwino <s.garcia@applicaster.com>
2021-01-04 17:04:12 -0500

    fix: forgot to re-add build script, now minified

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,71 +1,17 @@
 // These files are being merged into to the index.js file in public
-const createList = (sectionData, id) => {
-  const list = document.createElement("ul");
-  list.id = id + "-list";
-  list.className = "section-list";
-  sectionData.list.forEach(listItem => {
-    const item = document.createElement("li");
-    item.className = "section-item";
-    item.innerHTML = listItem;
-    list.appendChild(item);
-  });
-  return list;
-};
+const createList=(a,b)=>{const c=document.createElement("ul");return c.id=b+"-list",c.className="section-list",a.list.forEach(a=>{const b=document.createElement("li");b.className="section-item",b.innerHTML=a,c.appendChild(b)}),c};
 // These files are being merged into to the index.js file in public
-const createMessage = (sectionData, id) => {
-  const message = document.createElement("p");
-  message.id = id + "-message";
-  message.className = "section-message";
-  message.innerHTML = sectionData.message;
-  return message;
-};
-const mainBody = document.getElementById("main");
-
-const createSection = sectionData => {
-  if (sectionData && sectionData.featured) {
-    const id = sectionData?.id;
-    const section = document.createElement("section");
-    const title = sectionData.title && createTitle(sectionData, id);
-    const message = sectionData.message && createMessage(sectionData, id);
-    const list = sectionData.list && createList(sectionData, id);
-    section.id = id;
-    section.className = "floating-section";
-    title && section.appendChild(title);
-    message && section.appendChild(message);
-    list && section.appendChild(list);
-    mainBody.appendChild(section);
-  }
-};
-const createTitle = (sectionData, id) => {
-  const title = document.createElement("h4");
-  title.id = id + "-title";
-  title.className = "section-title";
-  title.innerHTML = sectionData.title;
-  return title;
-};
-const getHomeData = firebase.database().ref("/home").once("value");
-
-const renderSections = sections => {
-  sections.forEach(section => {
-    createSection(section);
-  });
-}; // const renderLists = (...lists) => {
+const createMessage=(a,b)=>{const c=document.createElement("p");return c.id=b+"-message",c.className="section-message",c.innerHTML=a.message,c};
+const mainBody=document.getElementById("main"),createSection=a=>{if(a&&a.featured){const b=a?.id,c=document.createElement("section"),d=a.title&&createTitle(a,b),e=a.message&&createMessage(a,b),f=a.list&&createList(a,b);c.id=b,c.className="floating-section",d&&c.appendChild(d),e&&c.appendChild(e),f&&c.appendChild(f),mainBody.appendChild(c)}};
+const createTitle=(a,b)=>{const c=document.createElement("h4");return c.id=b+"-title",c.className="section-title",c.innerHTML=a.title,c};
+const getHomeData=firebase.database().ref("/home").once("value"),renderSections=a=>{a.forEach(a=>{createSection(a)})};// const renderLists = (...lists) => {
 //   for (let i = 0; i < lists.length; i++) {
 //     CreateList(lists[i]);
 //   }
 // };
-
-
-getHomeData.then(snapshot => {
-  const data = snapshot?.val();
-
-  if (data && data?.sections) {
-    const sortedSections = data.sections.sort((a, b) => a?.order > b?.order);
-    renderSections(sortedSections);
-  } // if (data.lists) {
-  //   renderLists(data.lists);
-  // }
-
+getHomeData.then(a=>{const b=a?.val();if(b&&b?.sections){const a=b.sections.sort((c,a)=>c?.order>a?.order);renderSections(a)}// if (data.lists) {
+//   renderLists(data.lists);
+// }
 });
 
 //# sourceMappingURL=index.js.map
